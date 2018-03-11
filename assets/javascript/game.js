7a67df01a7ce636ac92bf17ab02b9d1d5ca93e0b
Joan Marie Verba <verba001@umn.edu>
2018-03-11 16:34:22 -0500

    basic formatting

---
+++ assets/javascript/game.js
@@ -0,0 +1,108 @@
+"use strict"
+
+let hp1 = 127;
+let hp2 = 103;
+let hp3 = 179;
+let hp4 = 151;
+
+
+$("#health1").text(hp1);
+$("#health2").text(hp2);
+$("#health3").text(hp3);
+$("#health4").text(hp4);
+
+
+
+// //create function to get a random number between min and max, inclusive
+// function getRandomInclusive(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+//
+// let targetValue = 0;
+// let currentScore = 0;
+// let numberOfWins = 0;
+// let numberOfLosses = 0;
+//
+// let blue = 0;
+// let diamond = 0;
+// let purple = 0;
+// let yellow = 0;
+//
+// let done = false;
+//
+// // assign random numbers to target and crystals
+// targetValue = getRandomInclusive(19, 120);
+// blue = getRandomInclusive(1, 12);
+// diamond = getRandomInclusive(1, 12);
+// purple = getRandomInclusive(1, 12);
+// yellow = getRandomInclusive(1, 12);
+//
+// $("#target").text(targetValue);
+// $("#score").text(currentScore);
+//
+// $("#numwins").text(`Number of wins: ${numberOfWins}`);
+// $("#numlosses").text(`Number of losses: ${numberOfLosses}`);
+//
+// function reset () {
+//     currentScore = 0;
+//     done = false;
+//     // assign random numbers to target and crystals
+//     targetValue = getRandomInclusive(19, 120);
+//     blue = getRandomInclusive(1, 12);
+//     diamond = getRandomInclusive(1, 12);
+//     purple = getRandomInclusive(1, 12);
+//     yellow = getRandomInclusive(1, 12);
+//
+//     $("#target").text(targetValue);
+//     $("#score").text(currentScore);
+//     $("#numwins").text(`Number of wins: ${numberOfWins}`);
+//     $("#numlosses").text(`Number of losses: ${numberOfLosses}`);
+// }
+//
+//
+// //blue button clicked
+// $(".btn1").on("click", function () {
+//     $("#winorlossnotice").empty();
+//     currentScore += blue;
+//     $("#score").text(currentScore);
+//     checkForEndOfGame ();
+// });
+//
+// //diamond button clicked
+// $(".btn2").on("click", function () {
+//     $("#winorlossnotice").empty();
+//     currentScore += diamond;
+//     $("#score").text(currentScore);
+//     checkForEndOfGame();
+// });
+//
+// //purple button clicked
+// $(".btn3").on("click", function () {
+//     $("#winorlossnotice").empty();
+//     currentScore += purple;
+//     $("#score").text(currentScore);
+//     checkForEndOfGame();
+// });
+//
+// //yellow button clicked
+// $(".btn4").on("click", function () {
+//     $("#winorlossnotice").empty();
+//     currentScore += yellow;
+//     $("#score").text(currentScore);
+//     checkForEndOfGame();
+// });
+//
+// function checkForEndOfGame () {
+//     if (currentScore === targetValue) {
+//         $("#winorlossnotice").text("Success!! You won! Game reset...try again");
+//         numberOfWins++;
+//         $("#numwins").text(`Number of wins: ${numberOfWins}`);
+//         reset ();
+//     }
+//     if (currentScore > targetValue) {
+//         $("#winorlossnotice").text("Sorry!! You lost! Game reset...try again");
+//         numberOfLosses++;
+//         $("#numlosses").text(`Number of losses: ${numberOfLosses}`);
+//         reset ();
+//     }
+// }
